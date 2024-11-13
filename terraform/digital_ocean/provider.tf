7bade816ab8e89a2db784b607d63404799242e64
lily4499 <konissil@gmail.com>
2024-11-13 15:05:28 -0500

    setup digital ocean

--- terraform/digital_ocean/provider.tf
+++ terraform/digital_ocean/provider.tf
@@ -1,14 +1,12 @@
-# Specify the DigitalOcean provider
-provider "digitalocean" {
-  token = env.DIGITALOCEAN_TOKEN
-}
-
-
 terraform {
   required_providers {
     digitalocean = {
-      source  = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"  # Correct source for DigitalOcean provider
       version = "~> 2.0"  # Adjust the version as needed
     }
   }
+}
+
+provider "digitalocean" {
+  token = env.DIGITALOCEAN_TOKEN  # Make sure this environment variable is set
 }
