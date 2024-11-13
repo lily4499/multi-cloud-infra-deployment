0244ece7e5f80db65a3f0d26f8cf30370625db55
lily4499 <konissil@gmail.com>
2024-11-13 16:34:36 -0500

    setup digital ocean

--- modules/droplet/outputs.tf
+++ modules/droplet/outputs.tf
@@ -1,9 +1,9 @@
-output "droplet_ips" {
-  description = "Public IP addresses of the Droplets"
-  value       = [for droplet in digitalocean_droplet.this : droplet.ipv4_address]
+output "droplet_ip" {
+  value       = digitalocean_droplet.this.ipv4_address
+  description = "The IP address of the created droplet"
 }
 
-output "droplet_ids" {
-  description = "IDs of the Droplets"
-  value       = [for droplet in digitalocean_droplet.this : droplet.id]
+output "droplet_id" {
+  value       = digitalocean_droplet.this.id
+  description = "The ID of the created droplet"
 }
