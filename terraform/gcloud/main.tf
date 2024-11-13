72d6dda5af27442cbb3fe754b4e8f4a4da575941
lily4499 <konissil@gmail.com>
2024-11-13 10:05:25 -0500

    setup gcloud

--- terraform/gcloud/main.tf
+++ terraform/gcloud/main.tf
@@ -57,15 +57,3 @@ module "gke" {
 
 
 
-
-
-
-# GKE Cluster Module
-module "gke" {
-  source        = "../../modules/gke_cluster"
-  cluster_name  = var.gke_cluster_name
-  node_count    = var.node_count
-  machine_type  = var.machine_type
-}
-
-# GCR (Configured with `terraform.tfvars` for authentication)
--- terraform/gcloud/outputs.tf
+++ terraform/gcloud/outputs.tf
@@ -39,18 +39,3 @@ output "gke_cluster_endpoint" {
 
 
 
-
-
-
-
--------------------------------------
-
-
-
-
-# Outputs for resources created by the modules
-
-output "gke_cluster_name" {
-  description = "The name of the GKE cluster"
-  value       = module.gke.cluster_name
-}
