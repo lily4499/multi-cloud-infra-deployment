b6b498b067ae16ae8cf548255d08e6fcac51da83
lily4499 <konissil@gmail.com>
2024-11-13 16:30:30 -0500

    setup digital ocean

--- terraform/digital_ocean/outputs.tf
+++ terraform/digital_ocean/outputs.tf
@@ -35,7 +35,7 @@ output "kubernetes_cluster_name" {
 
 output "kubeconfig" {
   description = "Kubeconfig content to access the Kubernetes cluster"
-  value = digitalocean_kubernetes_cluster.do_cluster.kube_config[0].raw_config
+  value       = module.kubernetes_cluster.kubeconfig
   sensitive   = true
 }
 
